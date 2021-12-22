a3b98b9d2afdbab9898f77a010ebe5f79f5f6c98
KY <55354225+okyspace@users.noreply.github.com>
2021-12-22 17:09:51 +0800

    added deployment strategy (mirror)
    ssl3a  - normal 1 deployment
    ssl3b - mirror traffic case

--- triton-ingress-ssl/install-ingress.sh
+++ triton-ingress-ssl/install-ingress.sh
@@ -4,7 +4,13 @@ sh ssl01-secret-triton.sh
 
 kubectl apply -f ssl02-issuer-triton.yaml
 
-kubectl apply -f ssl03-ingress-triton.yaml
+# kubectl apply -f ssl03b-ingress-triton-api.yaml
+# kubectl apply -f ssl03b-ingress-triton-grpc.yaml
+# kubectl apply -f ssl03b-ingress-triton-metrics.yaml
+
+kubectl apply -f ssl03a-ingress-triton-api.yaml
+kubectl apply -f ssl03a-ingress-triton-grpc.yaml
+kubectl apply -f ssl03a-ingress-triton-metrics.yaml
 
 
 
